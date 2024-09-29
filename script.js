9e668d618ac787438d3a24b5abae002f5c33a791
Tabish Raza <tabishraza586@gmail.com>
2024-09-29 12:28:34 +0530

    week-3 easy task

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+//DOM
+//document.getElementById("red").style.backgroundColor = red;
+
+function red() {
+  document.body.style.backgroundColor = "red";
+}
+
+function yellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+function black() {
+  document.body.style.backgroundColor = "black";
+}
+function purple() {
+  document.body.style.backgroundColor = "purple";
+}
+function green() {
+  document.body.style.backgroundColor = "green";
+}
+function blue() {
+  document.body.style.backgroundColor = "blue";
+}
+function default1() {
+  document.body.style.backgroundColor = "white";
+}
